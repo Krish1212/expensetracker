4aa72fd90f31f8b279f4d56ec1d36f0b6ded44f5
Krish <krishnamoorthy@gpsl.co>
2023-08-21 11:00:38 +0530

    secure setting

--- pages/test.php
+++ pages/test.php
@@ -3,17 +3,23 @@
 use Exception;
 use MongoDB\Driver\Manager;
 
+$uname = 'sadmin';
+$upass = 'sadmin';
+$uri = 'mongodb+srv://' . $uname . ':' . $upass . '@cluster0.5ufcvsy.mongodb.net/?retryWrites=true&w=majority&authSource=expenseData&authMechanism=SCRAM-SHA-1';
+
+$manager = new MongoDB\Driver\Manager($uri);
+$command = new MongoDB\Driver\Command(['count' => 'collection']);
 try
 {
-    $uri = 'mongodb+srv://sadmin:sadmin@cluster0.5ufcvsy.mongodb.net/?retryWrites=true&w=majority&authSource=expenseData&authMechanism=SCRAM';
+    $cursor = $manager->executeCommand('db', $command);
     echo "Pinged your deployment. You successfully connected to MongoDB!\n";
 }
 catch (Exception $e)
 {
     printf($e->getMessage());
+    exit;
 }
-
-$manager = new MongoDB\Driver\Manager($uri);
-var_dump($manager);
+$response = $cursor->toArray()[0];
+var_dump($response);
 
 ?>
